567b5aa658d3d02602d572e1922d00a72d21ee8c
vb4370 <victoria.bigeagle@students.msswarriors.org>
2026-01-29 14:26:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
-let output = "quoteLength" + [twentySecondLetter];
+let output = "quoteLength = quote.length;" + "twentySecondLetter= quote[22]";
 
 document.getElementById('result').innerHTML = output;
 //Add code below
