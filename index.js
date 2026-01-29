e8c9ccd13df85bfbaa27b32e342b7b2f2662d215
vb4370 <victoria.bigeagle@students.msswarriors.org>
2026-01-29 14:17:38 -0600

    does this commit work

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
+let output = "quoteLength" + [twentySecondLetter];
+
+document.getElementById('result').innerHTML = output;
 //Add code below
